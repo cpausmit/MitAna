de15c72a14208bb9d300ef1138f9ef0e81327230
Siddharth <sidn@mit.edu>
2015-06-29 04:21:49 -0400

    Added tree fillers, physics utils

---
+++ PhysicsUtils/interface/QGTagger.h
@@ -0,0 +1,37 @@
+#ifndef MITPHYSICS_UTILS_QGTAGGER_H
+#define MITPHYSICS_UTILS_QGTAGGER_H
+
+#include "MitAna/DataTree/interface/PFJetCol.h"
+#include "MitAna/DataTree/interface/VertexCol.h"
+#include "MitAna/PhysicsUtils/interface/QGLikelihoodCalculator.h"
+#include <map>
+
+namespace mithep {
+
+  class QGTagger {
+
+  public:
+    QGTagger(bool useCHS);
+    virtual ~QGTagger();
+    
+    // setters
+    void SetRhoIso(Float_t rhoIso) { variables["rhoIso"] = rhoIso; }
+
+    // compute the inputs for gluon-quark discrimination
+    void CalculateVariables(const PFJet *jet, const VertexCol *vertices);
+    
+    // getters
+    Float_t QGValue(); 
+    Float_t GetPtD(); 
+    Float_t GetAxis1(); 
+    Float_t GetAxis2(); 
+    Float_t GetMult(); 
+
+  private:
+    QGLikelihoodCalculator     *qgLikelihood;
+    std::map<TString, Float_t>  variables;
+
+    ClassDef(QGTagger, 1)
+  };
+}
+#endif
