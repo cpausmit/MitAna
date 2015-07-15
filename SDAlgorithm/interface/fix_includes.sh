bcb4fdd4a69dd51843de070563906a381b6cf796
Sid Narayanan <sidn@mit.edu>
2015-07-15 02:06:19 +0200

    removed SD algo code from this repo

--- SDAlgorithm/interface/fix_includes.sh
+++
@@ -1,7 +0,0 @@
-for file in *.h
-do
-  for rule in *.h    
-  do
-    sed -i "s:\"$rule\":\"ShowerDeconstruction/SDAlgorithm/interface/$rule\":" "$file"
-  done
-done
--- SDAlgorithm/src/fix_includes.sh
+++
@@ -1,8 +0,0 @@
-for file in *.cc
-do
-  for include in ../interface/*.h    
-  do
-    export rule=`basename $include`
-    sed -i "s:\"$rule\":\"ShowerDeconstruction/SDAlgorithm/interface/$rule\":" "$file"
-  done
-done
