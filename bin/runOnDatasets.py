88d407b01c08f668bcce495ee5cd7f2f8974c214
Yutaro Iiyama <yiiyama@mit.edu>
2015-08-05 14:02:15 -0400

    bugfix again

--- bin/runOnDatasets.py
+++ bin/runOnDatasets.py
@@ -392,7 +392,7 @@ def writeMacros(datasets, env):
         # get the dataset file list
         ds = catalog.FindDataset(book, dataset, '', 1)
         firstFile = ds.FileUrl(0)
-        if fistFile == '':
+        if firstFile == '':
             print ' Dataset ' + book + '/' + dataset + ' appears to be empty.'
             continue
 
