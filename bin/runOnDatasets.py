7c241974fd26c2e4a2cdbd44af7500e82baadcfd
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2015-08-10 01:42:17 +0200

    x509 is optional
    Do we actually need x509 proxy in our jobs though?
    In any case, now checking that the file exists in the workspace before adding it to transfer_input_files.

--- bin/runOnDatasets.py
+++ bin/runOnDatasets.py
@@ -614,6 +614,8 @@ def writeCondorConf(inTemplatePath, env):
 
     for inputFile in ['{book}/{dataset}/run.py', env.cmsswPack, 'env.sh', 'x509up']:
         fullPath = env.workspace + '/' + inputFile
+        if inputFile == 'x509up' and not os.path.exists(fullPath):
+            continue
         if fullPath not in inputList:
             if listWasGiven:
                 print ' Adding', fullPath, 'to transfer_input_files.'
