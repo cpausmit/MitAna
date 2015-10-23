71bcd75e6b1018ec45c87a45179c58d09c734f91
Yutaro Iiyama <yiiyama@mit.edu>
2015-10-23 06:20:56 -0400

    adding new features to physics modules

--- PhysicsMod/src/FilterMod.cc
+++ PhysicsMod/src/FilterMod.cc
@@ -1,5 +1,34 @@
 #include "MitAna/PhysicsMod/interface/FilterMod.h"
+#include "MitAna/DataTree/interface/PFCandidate.h"
+#include "MitAna/DataTree/interface/MCParticle.h"
+#include <algorithm>
 
 using namespace mithep;
 
 templateClassImp(mithep::FilterMod)
+
+template<>
+Bool_t
+mithep::FilterMod<mithep::PFCandidate, mithep::PFCandidate>::IsAllowedParticleId(mithep::PFCandidate const& part) const
+{
+  if (fParticleIds.size() == 0)
+    return true;
+
+  int id = part.PFType();
+  if (part.Charge() < 0.)
+    id *= -1;
+
+  bool inlist = std::binary_search(fParticleIds.begin(), fParticleIds.end(), id);
+  return fVetoParticleId ? !inlist : inlist;
+}
+
+template<>
+Bool_t
+mithep::FilterMod<mithep::MCParticle, mithep::MCParticle>::IsAllowedParticleId(mithep::MCParticle const& part) const
+{
+  if (fParticleIds.size() == 0)
+    return true;
+
+  bool inlist = std::binary_search(fParticleIds.begin(), fParticleIds.end(), part.PdgId());
+  return fVetoParticleId ? !inlist : inlist;
+}
