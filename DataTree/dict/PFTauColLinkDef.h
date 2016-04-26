099c78cedd642723550fc7d075fece78a81d0584
Yutaro Iiyama <yiiyama@mit.edu>
2016-04-26 14:43:32 -0400

    removing comments

--- DataTree/dict/PFTauColLinkDef.h
+++ DataTree/dict/PFTauColLinkDef.h
@@ -23,6 +23,7 @@
 // fPFTauLegacyDiscriminator: array branch with a "!" (not written to file)
 // For those branches, pragma read target only recognizes the array name, but GetDataMemberOffset requires the full array length.
 // Therefore need to setup the target object by hand in the user code below.
+
 #pragma read \
     sourceClass="mithep::PFTau" \
     version="[-7]" \
@@ -65,8 +66,9 @@
             Double32_t fTightMuonRejection2;" \
     targetClass="mithep::PFTau" \
     target="fPFTauDiscriminants, fPFTauIdentifiers, fPFTauLegacyDiscriminator" \
-    code="{  if (&fPFTauLegacyDiscriminator); \
-      static Long_t actualOffset = cls->GetDataMemberOffset(\"fPFTauLegacyDiscriminator[44]\"); \
+    code="{ if (&fPFTauLegacyDiscriminator); \
+      static const TString bname(Form(\"fPFTauLegacyDiscriminator[%d]\", mithep::PFTau::nLegacyDiscriminators)); \
+      static Long_t actualOffset = cls->GetDataMemberOffset(bname); \
       fPFTauLegacyDiscriminator_t& fActualPFTauLegacyDiscriminator = *(fPFTauLegacyDiscriminator_t*)(target + actualOffset); \
       auto setIdentifier([&fPFTauIdentifiers](UInt_t idx, Double_t value) { fPFTauIdentifiers.SetBit(idx - mithep::PFTau::nDiscriminants, value > 0.5); }); \
       auto setLegacy([&fActualPFTauLegacyDiscriminator](UInt_t idx, Double_t value) { fActualPFTauLegacyDiscriminator[idx - mithep::PFTau::nDiscriminants] = value; }); \
@@ -108,105 +110,16 @@
       setLegacy(mithep::PFTau::kDiscriminationByMediumMuonRejection2, onfile.fMediumMuonRejection2); \
       setLegacy(mithep::PFTau::kDiscriminationByTightMuonRejection2, onfile.fTightMuonRejection2); }" \
 
-/* // old discriminators mapped to current identifiers */
-/* #pragma read \ */
-/*     sourceClass="mithep::PFTau" \ */
-/*     version="[-7]" \ */
-/*     source="Double32_t fDiscriminationByDecayModeFinding; \ */
-/*             Double32_t fLooseCombinedIsolationDBSumPtCorr3Hits; \ */
-/*             Double32_t fMediumCombinedIsolationDBSumPtCorr3Hits; \ */
-/*             Double32_t fTightCombinedIsolationDBSumPtCorr3Hits;" \ */
-/*     targetClass="mithep::PFTau" \ */
-/*     target="fPFTauIdentifiers" \ */
-/*     code="{ auto set([&fPFTauIdentifiers](UInt_t idx, Double_t value) { fPFTauIdentifiers.SetBit(idx - mithep::PFTau::nDiscriminants, value > 0.5); }); \ */
-/*       set(mithep::PFTau::iDecayModeFinding, onfile.fDiscriminationByDecayModeFinding); \ */
-/*       set(mithep::PFTau::iByLooseCombinedIsolationDeltaBetaCorr3Hits, onfile.fLooseCombinedIsolationDBSumPtCorr3Hits); \ */
-/*       set(mithep::PFTau::iByMediumCombinedIsolationDeltaBetaCorr3Hits, onfile.fMediumCombinedIsolationDBSumPtCorr3Hits); \ */
-/*       set(mithep::PFTau::iByTightCombinedIsolationDeltaBetaCorr3Hits, onfile.fTightCombinedIsolationDBSumPtCorr3Hits); }" \ */
-
-/* // old discriminators mapped to legacy discriminators */
-/* #pragma read \ */
-/*     sourceClass="mithep::PFTau" \ */
-/*     version="[-7]" \ */
-/*     source="Double32_t fDiscriminationAgainstElectron; \ */
-/*             Double32_t fDiscriminationAgainstMuon; \ */
-/*             Double32_t fDiscriminationByMVAElectronRejection; \ */
-/*             Double32_t fDiscriminationByVLooseIsolation; \ */
-/*             Double32_t fDiscriminationByLooseIsolation; \ */
-/*             Double32_t fDiscriminationByMediumIsolation; \ */
-/*             Double32_t fDiscriminationByTightIsolation; \ */
-/*             Double32_t fMVA2rawElectronRejection; \ */
-/*             Double32_t fMVA2rawElectronRejectionCategory; \ */
-/*             Double32_t fMVA2LooseElectronRejection; \ */
-/*             Double32_t fMVA2MediumElectronRejection; \ */
-/*             Double32_t fMVA2TightElectronRejection; \ */
-/*             Double32_t fMVA3rawElectronRejection; \ */
-/*             Double32_t fMVA3rawElectronRejectionCategory; \ */
-/*             Double32_t fMVA3LooseElectronRejection; \ */
-/*             Double32_t fMVA3MediumElectronRejection; \ */
-/*             Double32_t fMVA3TightElectronRejection; \ */
-/*             Double32_t fMVA3VTightElectronRejection; \ */
-/*             Double32_t fDiscriminationByLooseElectronRejection; \ */
-/*             Double32_t fDiscriminationByMediumElectronRejection; \ */
-/*             Double32_t fDiscriminationByTightElectronRejection; \ */
-/*             Double32_t fDiscriminationByLooseMuonRejection; \ */
-/*             Double32_t fDiscriminationByMediumMuonRejection; \ */
-/*             Double32_t fDiscriminationByTightMuonRejection; \ */
-/*             Double32_t fDiscriminationByVLooseCombinedIsolationDBSumPtCorr; \ */
-/*             Double32_t fDiscriminationByLooseCombinedIsolationDBSumPtCorr; \ */
-/*             Double32_t fDiscriminationByMediumCombinedIsolationDBSumPtCorr; \ */
-/*             Double32_t fDiscriminationByTightCombinedIsolationDBSumPtCorr; \ */
-/*             Double32_t fDiscriminationByRawCombinedIsolationDBSumPtCorr; \ */
-/*             Double32_t fLooseMuonRejection2; \ */
-/*             Double32_t fMediumMuonRejection2; \ */
-/*             Double32_t fTightMuonRejection2;" \ */
-/*     targetClass="mithep::PFTau" \ */
-/*     target="fPFTauLegacyDiscriminator" \ */
-/*     code="{ auto set([&fPFTauLegacyDiscriminator](UInt_t idx, Double_t value) { fPFTauLegacyDiscriminator[idx - mithep::PFTau::nDiscriminants] = value; }); \ */
-/*       set(mithep::PFTau::kDiscriminationAgainstElectron, onfile.fDiscriminationAgainstElectron); \ */
-/*       set(mithep::PFTau::kDiscriminationAgainstMuon, onfile.fDiscriminationAgainstMuon); \ */
-/*       set(mithep::PFTau::kDiscriminationByMVAElectronRejection, onfile.fDiscriminationByMVAElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByVLooseIsolation, onfile.fDiscriminationByVLooseIsolation); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseIsolation, onfile.fDiscriminationByLooseIsolation); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumIsolation, onfile.fDiscriminationByMediumIsolation); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightIsolation, onfile.fDiscriminationByTightIsolation); \ */
-/*       set(mithep::PFTau::kMVA2rawElectronRejection, onfile.fMVA2rawElectronRejection); \ */
-/*       set(mithep::PFTau::kMVA2rawElectronRejectionCategory, onfile.fMVA2rawElectronRejectionCategory); \ */
-/*       set(mithep::PFTau::kMVA2LooseElectronRejection, onfile.fMVA2LooseElectronRejection); \ */
-/*       set(mithep::PFTau::kMVA2MediumElectronRejection, onfile.fMVA2MediumElectronRejection); \ */
-/*       set(mithep::PFTau::kMVA2TightElectronRejection, onfile.fMVA2TightElectronRejection); \ */
-/*       set(mithep::PFTau::kMVA3rawElectronRejection, onfile.fMVA3rawElectronRejection); \ */
-/*       set(mithep::PFTau::kMVA3rawElectronRejectionCategory, onfile.fMVA3rawElectronRejectionCategory); \ */
-/*       set(mithep::PFTau::kMVA3LooseElectronRejection, onfile.fMVA3LooseElectronRejection); \ */
-/*       set(mithep::PFTau::kMVA3MediumElectronRejection, onfile.fMVA3MediumElectronRejection); \ */
-/*       set(mithep::PFTau::kMVA3TightElectronRejection, onfile.fMVA3TightElectronRejection); \ */
-/*       set(mithep::PFTau::kMVA3VTightElectronRejection, onfile.fMVA3VTightElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseElectronRejection, onfile.fDiscriminationByLooseElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumElectronRejection, onfile.fDiscriminationByMediumElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightElectronRejection, onfile.fDiscriminationByTightElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseMuonRejection, onfile.fDiscriminationByLooseMuonRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumMuonRejection, onfile.fDiscriminationByMediumMuonRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightMuonRejection, onfile.fDiscriminationByTightMuonRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByVLooseCombinedIsolationDBSumPtCorr, onfile.fDiscriminationByVLooseCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseCombinedIsolationDBSumPtCorr, onfile.fDiscriminationByLooseCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumCombinedIsolationDBSumPtCorr, onfile.fDiscriminationByMediumCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightCombinedIsolationDBSumPtCorr, onfile.fDiscriminationByTightCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByRawCombinedIsolationDBSumPtCorr, onfile.fDiscriminationByRawCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseMuonRejection2, onfile.fLooseMuonRejection2); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumMuonRejection2, onfile.fMediumMuonRejection2); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightMuonRejection2, onfile.fTightMuonRejection2); }" \ */
-
-// fPFTauLegacyDiscriminator: array branch with a "!" (not written to file)
-// For those branches, pragma read target only recognizes the array name, but GetDataMemberOffset requires the full array length.
-// Therefore need to setup the target object by hand in the user code below.
+// See above for fPFTauLegacyDiscriminator
 #pragma read \
     sourceClass="mithep::PFTau" \
     version="[8]" \
     source="Double32_t fPFTauDiscriminator[30];" \
     targetClass="mithep::PFTau" \
     target="fPFTauDiscriminants, fPFTauIdentifiers, fPFTauLegacyDiscriminator" \
-    code="{  if (&fPFTauLegacyDiscriminator); \
-      static Long_t actualOffset = cls->GetDataMemberOffset(\"fPFTauLegacyDiscriminator[44]\"); \
+    code="{ if (&fPFTauLegacyDiscriminator); \
+      static const TString bname(Form(\"fPFTauLegacyDiscriminator[%d]\", mithep::PFTau::nLegacyDiscriminators)); \
+      static Long_t actualOffset = cls->GetDataMemberOffset(bname); \
       fPFTauLegacyDiscriminator_t& fActualPFTauLegacyDiscriminator = *(fPFTauLegacyDiscriminator_t*)(target + actualOffset); \
       enum OldDiscriminators { \
         lDiscriminationByLooseElectronRejection, \
@@ -273,97 +186,7 @@
       setLegacy(mithep::PFTau::kMVA3IsolationNeutralIsoPtSum, lMVA3IsolationNeutralIsoPtSum); \
       setLegacy(mithep::PFTau::kMVA3IsolationPUcorrPtSum, lMVA3IsolationPUcorrPtSum); }" \
 
-/* // old discriminators mapped to current identifiers */
-/* #pragma read \ */
-/*     sourceClass="mithep::PFTau" \ */
-/*     version="[8]" \ */
-/*     source="Double32_t fPFTauDiscriminator[30];" \ */
-/*     targetClass="mithep::PFTau" \ */
-/*     target="fPFTauIdentifiers" \ */
-/*     code="{ enum OldDiscriminators { \ */
-/*         lDiscriminationByLooseMuonRejection3 = 13, \ */
-/*         lDiscriminationByTightMuonRejection3 = 14, \ */
-/*         lDiscriminationByDecayModeFinding = 15, \ */
-/*         lDiscriminationByDecayModeFindingNewDMs = 16, \ */
-/*         lDiscriminationByLooseCombinedIsolationDBSumPtCorr3Hits = 23, \ */
-/*         lDiscriminationByMediumCombinedIsolationDBSumPtCorr3Hits = 24, \ */
-/*         lDiscriminationByTightCombinedIsolationDBSumPtCorr3Hits = 25, \ */
-/*       }; \ */
-/*       auto set([&fPFTauIdentifiers, &onfile](UInt_t idIdx, UInt_t disIdx) { fPFTauIdentifiers.SetBit(idIdx - mithep::PFTau::nDiscriminants, onfile.fPFTauDiscriminator[disIdx] > 0.5); }); \ */
-/*       set(mithep::PFTau::iAgainstMuonLoose3, lDiscriminationByLooseMuonRejection3); \ */
-/*       set(mithep::PFTau::iAgainstMuonTight3, lDiscriminationByTightMuonRejection3); \ */
-/*       set(mithep::PFTau::iDecayModeFinding, lDiscriminationByDecayModeFinding); \ */
-/*       set(mithep::PFTau::iDecayModeFindingNewDMs, lDiscriminationByDecayModeFindingNewDMs); \ */
-/*       set(mithep::PFTau::iByLooseCombinedIsolationDeltaBetaCorr3Hits, lDiscriminationByLooseCombinedIsolationDBSumPtCorr3Hits); \ */
-/*       set(mithep::PFTau::iByMediumCombinedIsolationDeltaBetaCorr3Hits, lDiscriminationByMediumCombinedIsolationDBSumPtCorr3Hits); \ */
-/*       set(mithep::PFTau::iByTightCombinedIsolationDeltaBetaCorr3Hits, lDiscriminationByTightCombinedIsolationDBSumPtCorr3Hits); }" \ */
-
-/* // old discriminators mapped to legacy discriminators */
-/* #pragma read \ */
-/*     sourceClass="mithep::PFTau" \ */
-/*     version="[8]" \ */
-/*     source="Double32_t fPFTauDiscriminator[30];" \ */
-/*     targetClass="mithep::PFTau" \ */
-/*     target="fPFTauLegacyDiscriminator" \ */
-/*     code="{ enum OldDiscriminators { \ */
-/*         lDiscriminationByLooseElectronRejection, \ */
-/*         lDiscriminationByMediumElectronRejection, \ */
-/*         lDiscriminationByTightElectronRejection, \ */
-/*         lDiscriminationByMVA5VLooseElectronRejection, \ */
-/*         lDiscriminationByMVA5LooseElectronRejection, \ */
-/*         lDiscriminationByMVA5MediumElectronRejection, \ */
-/*         lDiscriminationByMVA5TightElectronRejection, \ */
-/*         lDiscriminationByLooseMuonRejection, \ */
-/*         lDiscriminationByMediumMuonRejection, \ */
-/*         lDiscriminationByTightMuonRejection, \ */
-/*         lDiscriminationByLooseMuonRejection2, \ */
-/*         lDiscriminationByMediumMuonRejection2, \ */
-/*         lDiscriminationByTightMuonRejection2, \ */
-/*         lDiscriminationByLooseMuonRejection3, \ */
-/*         lDiscriminationByTightMuonRejection3, \ */
-/*         lDiscriminationByDecayModeFinding, \ */
-/*         lDiscriminationByDecayModeFindingNewDMs, \ */
-/*         lDiscriminationByDecayModeFindingOldDMs, \ */
-/*         lDiscriminationByVLooseCombinedIsolationDBSumPtCorr, \ */
-/*         lDiscriminationByLooseCombinedIsolationDBSumPtCorr, \ */
-/*         lDiscriminationByMediumCombinedIsolationDBSumPtCorr, \ */
-/*         lDiscriminationByTightCombinedIsolationDBSumPtCorr, \ */
-/*         lDiscriminationByRawCombinedIsolationDBSumPtCorr, \ */
-/*         lDiscriminationByLooseCombinedIsolationDBSumPtCorr3Hits, \ */
-/*         lDiscriminationByMediumCombinedIsolationDBSumPtCorr3Hits, \ */
-/*         lDiscriminationByTightCombinedIsolationDBSumPtCorr3Hits, \ */
-/*         lDiscriminationByRawCombinedIsolationDBSumPtCorr3Hits, \ */
-/*         lMVA3IsolationChargedIsoPtSum, \ */
-/*         lMVA3IsolationNeutralIsoPtSum, \ */
-/*         lMVA3IsolationPUcorrPtSum \ */
-/*       }; \ */
-/*       auto set([&fPFTauLegacyDiscriminator, &onfile](UInt_t legIdx, UInt_t disIdx) { fPFTauLegacyDiscriminator[legIdx - mithep::PFTau::nDiscriminators] = onfile.fPFTauDiscriminator[disIdx]; }); \ */
-/*       set(mithep::PFTau::kDiscriminationByMVA5VLooseElectronRejection, lDiscriminationByMVA5VLooseElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByMVA5LooseElectronRejection, lDiscriminationByMVA5LooseElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByMVA5MediumElectronRejection, lDiscriminationByMVA5MediumElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByMVA5TightElectronRejection, lDiscriminationByMVA5TightElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseElectronRejection, lDiscriminationByLooseElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumElectronRejection, lDiscriminationByMediumElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightElectronRejection, lDiscriminationByTightElectronRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseMuonRejection, lDiscriminationByLooseMuonRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumMuonRejection, lDiscriminationByMediumMuonRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightMuonRejection, lDiscriminationByTightMuonRejection); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseMuonRejection2, lDiscriminationByLooseMuonRejection2); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumMuonRejection2, lDiscriminationByMediumMuonRejection2); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightMuonRejection2, lDiscriminationByTightMuonRejection2); \ */
-/*       set(mithep::PFTau::kDiscriminationByDecayModeFindingOldDMs, lDiscriminationByDecayModeFindingOldDMs); \ */
-/*       set(mithep::PFTau::kDiscriminationByVLooseCombinedIsolationDBSumPtCorr, lDiscriminationByVLooseCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByLooseCombinedIsolationDBSumPtCorr, lDiscriminationByLooseCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByMediumCombinedIsolationDBSumPtCorr, lDiscriminationByMediumCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByTightCombinedIsolationDBSumPtCorr, lDiscriminationByTightCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kDiscriminationByRawCombinedIsolationDBSumPtCorr, lDiscriminationByRawCombinedIsolationDBSumPtCorr); \ */
-/*       set(mithep::PFTau::kMVA3IsolationChargedIsoPtSum, lMVA3IsolationChargedIsoPtSum); \ */
-/*       set(mithep::PFTau::kMVA3IsolationNeutralIsoPtSum, lMVA3IsolationNeutralIsoPtSum); \ */
-/*       set(mithep::PFTau::kMVA3IsolationPUcorrPtSum, lMVA3IsolationPUcorrPtSum); }" \ */
-
-// fPFTauLegacyDiscriminator: array branch with a "!" (not written to file)
-// For those branches, pragma read target only recognizes the array name, but GetDataMemberOffset requires the full array length.
-// Therefore need to setup the target object by hand in the user code below.
+// See above for fPFTauLegacyDiscriminator
 #pragma read \
     sourceClass="mithep::PFTau" \
     version="[9]" \
@@ -371,7 +194,8 @@
     targetClass="mithep::PFTau" \
     target="fPFTauDiscriminants, fPFTauIdentifiers, fPFTauLegacyDiscriminator" \
     code="{ if (&fPFTauLegacyDiscriminator); \
-      static Long_t actualOffset = cls->GetDataMemberOffset(\"fPFTauLegacyDiscriminator[44]\"); \
+      static const TString bname(Form(\"fPFTauLegacyDiscriminator[%d]\", mithep::PFTau::nLegacyDiscriminators)); \
+      static Long_t actualOffset = cls->GetDataMemberOffset(bname); \
       fPFTauLegacyDiscriminator_t& fActualPFTauLegacyDiscriminator = *(fPFTauLegacyDiscriminator_t*)(target + actualOffset); \
       enum OldDiscriminators { \
         lDiscriminationByMVA5VLooseElectronRejection, \
